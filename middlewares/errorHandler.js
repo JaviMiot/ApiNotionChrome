daedcab9bad0453b39ac0e1f8754abf0e2ee91b3
Javier Manobanda <51manobandajavier@gmail.com>
2021-11-08 20:43:13 -0500

    :sparkles: add middlewares
    add errorhandlere middlewares

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,14 @@
+function errorLogs(error, req, res, next) {
+  console.log(error);
+  next(error);
+}
+
+
+function errorHandler(error, req, res, next) {
+  res.status('500').json({
+    message: error.message,
+    stack: error.stack,
+  });
+}
+
+module.exports = { errorLogs, errorHandler };
